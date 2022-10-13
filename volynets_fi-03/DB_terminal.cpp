878c1a6743997b03a96af4062a9b840d69bffeab
serhii023 <serhii023@gmail.com>
2022-10-13 12:42:06 +0300

    update DB_terminal

--- volynets_fi-03/DB_terminal.cpp
+++ volynets_fi-03/DB_terminal.cpp
@@ -8,28 +8,20 @@ void DB_terminal::start(){
     nextCommand();
 }
 
-void DB_terminal::nextCommand(){
+std::string DB_terminal::nextCommand(){
     std::cout << ">>";
 
-    Token t = Token(_COMMAND_TOKEN_);
-    char c;
-    while(t.getValue() != _EOC_TOKEN_){
-        cin.get(c);
-        if(c == ','){
-            for(std::string s: COMMAND_TYPES){
-                if(t.getValue() == "f")
-            }
-        }
-        
-        if(c != ' ' || c != ','){
-            t.pushValue(c);
-        }
-    }
+    std::string command = "";
+    getline(std::cin, command,';');
+
+    return command.append(";");
 }
 
 
 Token::Token(std::string type): type(type){
 }
+
+Token::Token(std::string type, std::string value): type(type), value(value){}
 
 std::string Token::getType(){
     return type;
@@ -52,15 +44,168 @@ Token Token::setType(std::string type){
 }
 
 Token Token::pushValue(char symbol){
-    this->value += std::string(symbol);
+    this->value += std::string(1, symbol);
 
     return *this;
 }
 
 std::string toUpperCase(std::string s){
-    for(auto c: s){
-        c = toupper(c);
+    for(int i = 0; i < s.size(); ++i){
+        s.at(i) = toupper(s.at(i));
     }
 
     return s;
 }
+
+
+Interpreter::Interpreter(){}
+
+Token Interpreter::interpretToken(std::string token){
+    if(token == ","){
+        return Token(_COMMA_TOKEN_, token);
+    }
+
+    if(token == ";"){
+        return Token(_EOC_TOKEN_, token);
+    }
+
+    if(token == "("){
+        return Token(_OPEN_BRACKET_TOKEN_, token);
+    }
+
+    if(token == ")"){
+        return Token(_CLOSE_BRACKET_TOKEN_, token);
+    }
+
+    if(token == ">" || token == ">=" || token == "==" || token == "<" || token == "<=" || token == "<>"){
+        return Token(_COMPARISON_TOKEN_, token);
+    }
+
+    if(isCorrectLiteral(token)){
+        for(auto i: COMMAND_TYPES){
+            if(toUpperCase(token) == i){
+                return Token(_COMMAND_TOKEN_, i);
+            }
+        }
+
+        return Token(_NAME_TOKEN_, token);
+    }
+
+    if(token.at(0) == '"' && token.at(token.size()-1) == '"' && token.size() > 2){
+        if(isCorrectLiteral(token.substr(1, token.size()-2))){
+            return Token(_VALUE_TOKEN_, token);
+        }
+    }
+
+    return Token(_UNDEFINED_TOKEN_, token);
+}
+
+std::vector<std::string> Interpreter::interpretCommand(std::string str_comm){
+    std::vector<std::string> vec_comm;
+    std::string temp = "";
+    char prew_c = " ";
+
+    for(int i = 0; i < str_comm.size(); ++i){
+        if(str_comm.at(i) == ' '){
+            
+        }
+    }
+    
+    return
+}
+
+
+
+// std::vector<Token> Interpreter::interpretCommand(std::string command){
+//     std::vector<Token> command_t;
+    
+//     Token cur_token = Token(_EMPTY_TOKEN_);
+//     int l = command.size() - 1; // because we don't need to rcognise the last symbol, that is ';'
+    
+//     try{
+//         for(int i = 0; i < l; ++i){
+//             if(std::isalpha(command.at(i)) || command.at(i) == '"' || command.at(i) == '_'){ // Do if text
+//                 if(cur_token.getValue() == " "){
+//                     cur_token.setValue(std::string(1, command.at(i)));
+//                 }else{
+//                     cur_token.pushValue(command.at(i));
+//                 }
+//             }
+            
+//             if(command.at(i) == ' '){ // Do if space
+//                 if(cur_token.getValue() != " " && cur_token.getValue() != ""){
+//                     interpretToken(cur_token);
+//                     command_t.push_back(cur_token);
+//                     cur_token = Token(_EMPTY_TOKEN_);
+//                 }
+//                 cur_token.setValue(" ");
+//             }
+
+//             if(command.at(i) == ','){  // Do if comma
+//                 interpretToken(cur_token);
+//                 command_t.push_back(cur_token);
+//                 command_t.push_back(Token(_COMMA_TOKEN_, ","));
+//                 cur_token = Token(_EMPTY_TOKEN_);
+//                 cur_token.setValue(" ");
+//             }
+
+//             if(command.at(i) == '('){ // Do if open bracket
+//                 if(cur_token.getValue() != " " && cur_token.getValue() != ""){
+//                     interpretToken(cur_token);
+//                     cur_token = Token(_EMPTY_TOKEN_);
+//                     cur_token.setValue(" ");
+//                 }
+//                 command_t.push_back(Token(_OPEN_BRACKET_TOKEN_, "("));
+//             }
+            
+//             if(command.at(i) == ')'){ // Do if close bracket
+//                 if(cur_token.getValue() != " " && cur_token.getValue() != ""){
+//                     interpretToken(cur_token);
+//                     cur_token = Token(_EMPTY_TOKEN_);
+//                     cur_token.setValue(" ");
+//                 }
+//                 command_t.push_back(Token(_CLOSE_BRACKET_TOKEN_, ")"));
+//             }
+//         }
+//     }catch(const char *e){
+//         std::cerr << e << '\n';
+//         throw e;
+//     }
+
+//     command_t.push_back(Token(_EOC_TOKEN_, ";"));
+//     return command_t;
+// }
+
+void printCommand(std::vector<Token> command){
+    std::cout << "Command:" << std::endl;
+    for(auto i: command){
+        std::cout << "\t" << i.getType() << ": " << i.getValue() << std::endl;
+    }
+    std::cout << "End of command." << std::endl;
+}
+
+bool isCorrectLiteral(const char c){
+    if(std::isalnum(c) || c == '_'){
+        return true;
+    }
+
+    return false;
+}
+
+bool isCorrectLiteral(std::string s){
+    if(std::isdigit(s.at(0))){
+        return false;
+    }
+
+    bool is_correct_iteral = true;
+    for(int i = 0; i < s.size(); ++i){
+        if(!isCorrectLiteral(s.at(i))){
+            is_correct_iteral = false;
+        }
+    }
+
+    return is_correct_iteral;
+}
+
+
+
--- volynets_fi-03/DB_terminal.h
+++ volynets_fi-03/DB_terminal.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cctype>
 #include <map>
 
 
@@ -10,6 +11,7 @@ class DB_terminal;
 class Token;
 class Interpreter;
 
+const std::string _TOKEN_ = "TOKEN";
 const std::string _VALUE_TOKEN_ = "VALUE";
 const std::string _COMMAND_TOKEN_ = "COMMAND";
 const std::string _NAME_TOKEN_ = "NAME";
@@ -19,8 +21,9 @@ const std::string _COMMA_TOKEN_ = "COMMA";
 const std::string _COMPARISON_TOKEN_ = "COMPARISON";
 const std::string _EOC_TOKEN_ = "EOC";
 const std::string _EMPTY_TOKEN_ = "EMPTY";
+const std::string _UNDEFINED_TOKEN_ = "UNDEFINED";
 
-std::vector<const std::string> COMMAND_TYPES{
+std::vector<std::string> COMMAND_TYPES{
     "CREATE",
     "INDEXED",
     "INSERT",
@@ -38,7 +41,7 @@ class DB_terminal{
 public:
     DB_terminal();
     void start();
-    void nextCommand();
+    std::string nextCommand();
 
 private:
     // Token current_token = Token(_EMPTY_TOKEN_);  // there is no ined in it yet
@@ -49,6 +52,7 @@ class Token{
 public:
     Token(){}
     Token(std::string type);
+    Token(std::string type, std::string value);
     std::string getType();
     std::string getValue();
     Token setValue(std::string value);
@@ -63,9 +67,14 @@ private:
 class Interpreter{
 public:
     Interpreter();
+    Token interpretToken(std::string token);
+    std::vector<std::string> interpretCommand(std::string command);
 };
 
 std::string toUpperCase(std::string s);
+void printCommand(std::vector<Token> command);
+bool isCorrectLiteral(const char c);
+bool isCorrectLiteral(std::string s);
 
 #include "DB_terminal.cpp"
 #endif
