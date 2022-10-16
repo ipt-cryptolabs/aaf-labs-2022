140dc11f20c10cdb389738353226414a185904e9
Solution <maxim.namchuk@gmail.com>
2022-10-16 18:09:58 +0300

    SELECT

--- Namchuk_FI-02_Khar_FI-02_var4/main.py
+++ Namchuk_FI-02_Khar_FI-02_var4/main.py
@@ -1,12 +1,13 @@
 import parser1
 
 if __name__ == "__main__":
+    command = None
     print('Enter:')
     line = input()
     while True:
         if line[-1] != ';':
             line = line + ' ' + input()
         else:
-            parser1.parse(line)
+            command = parser1.parse(line)
             print('Enter:')
             line = ' '
--- Namchuk_FI-02_Khar_FI-02_var4/parser1.py
+++ Namchuk_FI-02_Khar_FI-02_var4/parser1.py
@@ -47,16 +47,148 @@ def parse(line):
                 result_command.append(args)
                 print(result_command)
                 return result_command
-    elif re.match(r'(?i)(select\s+from)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(where)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\>\s+([a-zA-Z][a-zA-Z0-9_]*|\s*\"\s*.*\s*\")|\s+(?i)(join)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(on)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\=\s+[a-zA-Z][a-zA-Z0-9_]*))\s*\;', line):
-        split_command[-1] == split_command[-1][:-1]
-        args = re.findall(r'\".*?\"', line)
-        if args != []:
-            split_command[-1] = args[0]
-        print(split_command)
-        result_command.append('SELECT')
-        del split_command[0]
-        del split_command[0]
-        result_command.append(split_command[0])
-        del split_command[0]
-        print(split_command, result_command, sep='\n\n')
+    elif re.match(r'(?i)(select\s+from)\s+[a-zA-Z][a-zA-Z0-9_]*((|\s+(?i)(join)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(on)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\=\s+[a-zA-Z][a-zA-Z0-9_]*)|\s+(?i)(where)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\>\s+([a-zA-Z][a-zA-Z0-9_]*|\s*\"\s*.*\s*\"))|\s+(?i)(join)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(on)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\=\s+[a-zA-Z][a-zA-Z0-9_]*)\s+(?i)(where)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\>\s+([a-zA-Z][a-zA-Z0-9_]*|\s*\"\s*.*\s*\"))\s*\;',line):
+            split_command[-1] = split_command[-1][:-1]
+            args = re.findall(r'\".*?\"', line)
+            if args != []:
+                split_command[-1] = args[0]
+            #print(split_command)
+            if len(split_command)==3:
+                result_command.append("SELECT")
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                print(result_command)
+                return result_command
+            elif len(split_command)==5:
+                result_command.append("SELECT")
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("JOIN")
+                del split_command[0]
+                result_command.append(split_command[0])
+                print(result_command)
+                return result_command
+            elif len(split_command)==7:
+                result_command.append("SELECT")
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("WHERE")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                print(result_command)
+                return result_command
+            elif len(split_command)==9 and re.match(r'(?i)(on)', split_command[5]):
+                result_command.append("SELECT")
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("JOIN")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("ON")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                print(result_command)
+                return result_command
+            elif len(split_command)==9 and re.match(r'(?i)(where)', split_command[5]):
+                result_command.append("SELECT")
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("JOIN")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("WHERE")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                print(result_command)
+                return result_command
+            elif len(split_command)==13:
+                result_command.append("SELECT")
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("JOIN")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("ON")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                result_command.append("WHERE")
+                del split_command[0]
+                result_command.append(split_command[0])
+                del split_command[0]
+                del split_command[0]
+                result_command.append(split_command[0])
+                print(result_command)
+                return result_command
+            """args = re.findall(r'\".*?\"', line)
+            if args != []:
+                split_command[-1] = args[0]
+            print(split_command)
+            result_command.append('SELECT')
+            del split_command[0]
+            del split_command[0]
+            result_command.append(split_command[0])
+            del split_command[0]
+            if re.match(r'(?i)(join)', split_command[0]):
+                result_command.append('JOIN')
+                del split_command[0]
+                result_command.append(split_command[0])
+            print(split_command, result_command, sep='\n\n')"""
 # SELECT FROM cats WHERE name > "Murzi  )  k";
+# SELECT FROM cats;
+# SELECT FROM cats JOIN owners ON cat_owner_id = owner_id;
+"""SELECT FROM cats;
+[SELECT, cats]
+
+
+SELECT FROM cats 
+  WHERE name > “Murzik”;
+[SELECT, cats, WHERE, name, “Murzik”]
+
+
+SELECT FROM cats
+  JOIN owners ON cat_owner_id = owner_id;
+[SELECT, cats, JOIN, owners, ON, cat_owner_id, owner_id]
+
+
+SELECT FROM cats
+  JOIN owners;
+[SELECT, cats, JOIN, owners]
+
+
+SELECT FROM cats
+  JOIN owners ON cat_owner_id = owner_id
+WHERE name > “Murzik”;
+[SELECT, cats, JOIN, owners, ON, cat_owner_id, owner_id, WHERE, name, “Murzik”]
+
+
+SELECT FROM cats
+  JOIN owners
+WHERE name > “Murzik”;
+[SELECT, cats, JOIN, owners, WHERE, name, “Murzik”]"""
