845e1de30878f6e09c41f5bea7218c72326e8824
Khar Dmitry <qwedsazxc8250@gmail.com>
2022-10-17 22:08:46 +0300

    full? parser

--- Namchuk_FI-02_Khar_FI-02_var4/parser1.py
+++ Namchuk_FI-02_Khar_FI-02_var4/parser1.py
@@ -1,6 +1,4 @@
 import re
-
-
 def parse(line):
     line = line.lstrip()
     command = re.sub(r'\s+', ' ', line)
@@ -19,44 +17,69 @@ def parse(line):
             concatParameters += i
         if concatParameters == parameters:
             result_command.append("CREATE")
-            del split_command[0]
-            result_command.append(split_command[0])
-            del split_command[0]
-            split_command[0] = split_command[0].replace(split_command[0][0], '')
-            split_command[0] = split_command[0].replace(split_command[0][-1], '')
-            split_command[0] = split_command[0].replace(split_command[0][-1], '')
-            result_command.append(split_command[0].split(','))
-            print(result_command)
+            result_command.append(split_command[1])
+            split_command[2] = split_command[2].replace(split_command[2][0], '')
+            split_command[2] = split_command[2].replace(split_command[2][-1], '')
+            split_command[2] = split_command[2].replace(split_command[2][-1], '')
+            result_command.append(split_command[2].split(','))
             return result_command
     elif re.match(r'(?i)(insert|insert\s+into)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\(.+\)\s*\;', line):
         line = re.sub(r'\s*\)\s*;$', ',);', line)
         parenthesized_expression = re.findall(r'\(.*\)', line)
         parameters = parenthesized_expression[0][1:-1]
-        parenthesizedExpression = re.findall(r'\s*\"\s*\w+\s*\"\s*\,\s*', parameters)
+        parenthesizedExpression = re.findall(r'\s*\"\s*.+\s*\"\s*\,\s*', parameters)
         concatParameters = ''
         for i in parenthesizedExpression:
             concatParameters += i
         if concatParameters == parameters:
+            result_command.append('INSERT')
+            args = re.findall(r'\".*?\"', line)
+            i = 0
+            while i < len(args):
+                args[i] = args[i][1:-1]
+                i += 1
             if re.match(r'(?i)(insert\s+into)', line):
-                result_command.append('INSERT')
-                del split_command[0]
-                del split_command[0]
-                result_command.append(split_command[0])
-                del split_command[0]
-                args = re.findall(r'\".*?\"', line)
+                result_command.append(split_command[2])
                 result_command.append(args)
-                print(result_command)
-                return result_command
-    elif re.match(r'(?i)(select\s+from)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(where)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\>\s+([a-zA-Z][a-zA-Z0-9_]*|\s*\"\s*.*\s*\")|\s+(?i)(join)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(on)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\=\s+[a-zA-Z][a-zA-Z0-9_]*))\s*\;', line):
-        split_command[-1] == split_command[-1][:-1]
+            else:
+                result_command.append(split_command[1])
+                result_command.append(args)
+            return result_command
+    elif re.match(r'(?i)(select\s+from)\s+[a-zA-Z][a-zA-Z0-9_]*((|\s+(?i)(join)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(on)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\=\s+[a-zA-Z][a-zA-Z0-9_]*)|\s+(?i)(where)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\>\s+([a-zA-Z][a-zA-Z0-9_]*|\s*\"\s*.*\s*\"))|\s+(?i)(join)\s+[a-zA-Z][a-zA-Z0-9_]*(|\s+(?i)(on)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\=\s+[a-zA-Z][a-zA-Z0-9_]*)\s+(?i)(where)\s+[a-zA-Z][a-zA-Z0-9_]*\s+\>\s+([a-zA-Z][a-zA-Z0-9_]*|\s*\"\s*.*\s*\"))\s*\;',line):
+        split_command[-1] = split_command[-1][:-1]
         args = re.findall(r'\".*?\"', line)
         if args != []:
             split_command[-1] = args[0]
-        print(split_command)
-        result_command.append('SELECT')
-        del split_command[0]
-        del split_command[0]
-        result_command.append(split_command[0])
-        del split_command[0]
-        print(split_command, result_command, sep='\n\n')
-# SELECT FROM cats WHERE name > "Murzi  )  k";
+        result_command.append("SELECT")
+        result_command.append(split_command[2])
+        if len(split_command)==5:
+            result_command.append("JOIN")
+            result_command.append(split_command[4])
+        elif len(split_command)==7:
+            result_command.append("WHERE")
+            result_command.append(split_command[4])
+            result_command.append(split_command[6])
+        elif len(split_command)==9 and re.match(r'(?i)(on)', split_command[5]):
+            result_command.append("JOIN")
+            result_command.append(split_command[4])
+            result_command.append("ON")
+            result_command.append(split_command[6])
+            result_command.append(split_command[8])
+        elif len(split_command)==9 and re.match(r'(?i)(where)', split_command[5]):
+            result_command.append("JOIN")
+            result_command.append(split_command[4])
+            result_command.append("WHERE")
+            result_command.append(split_command[6])
+            result_command.append(split_command[8])
+        elif len(split_command)==13:
+            result_command.append("JOIN")
+            result_command.append(split_command[4])
+            result_command.append("ON")
+            result_command.append(split_command[6])
+            result_command.append(split_command[8])
+            result_command.append("WHERE")
+            result_command.append(split_command[10])
+            result_command.append(split_command[12])
+        return result_command
+    else:
+        return "Incorrect command syntax"
