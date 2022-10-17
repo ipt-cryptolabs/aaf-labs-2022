ca924ef3d52fd9ebc0301da24a45c9dce76337c1
Solution <maxim.namchuk@gmail.com>
2022-10-17 22:43:52 +0300

    parser + main

--- Namchuk_FI-02_Khar_FI-02_var4/main.py
+++ Namchuk_FI-02_Khar_FI-02_var4/main.py
@@ -1,5 +1,11 @@
 import parser1
 
+def create(command):
+    print(f"Table: {command[1]} with columns: {tuple(command[2])} was successfully created!")
+def insert(command):
+    print(f"Insert in {command[1]} values: {tuple(command[2])}")
+def select(command):
+    print(f"Select ready to use")
 if __name__ == "__main__":
     command = None
     print('Enter:')
@@ -9,5 +15,12 @@ if __name__ == "__main__":
             line = line + ' ' + input()
         else:
             command = parser1.parse(line)
+            if command[0] == 'CREATE':
+                create(command)
+            elif command[0] == 'INSERT':
+                insert(command)
+            elif command[0] == 'SELECT':
+                select(command)
             print('Enter:')
             line = ' '
+
