029a9fce4e9e6ee5ff26b1d14d37cccb7827c95d
bochok26 <97351597+bochok26@users.noreply.github.com>
2022-10-21 23:29:58 +0300

    init parser file and function file

---
+++ bochok_fb-03/functions.py
@@ -0,0 +1,13 @@
+def create(args):
+    print(f"The table {args[1]} has been created!")
+    return
+
+
+def insert(args):
+    print(f"1 row has been added to {args[1]}!")
+    return
+
+
+def select(args):
+    print(f"Selecting from {args[1]}...")
+    return
---
+++ bochok_fb-03/myparser1.py
@@ -0,0 +1,122 @@
+import re
+
+
+def make_normal(line):
+    line_wo_spaces = line
+    start = 0
+    is_found = False
+
+    while True:
+        line_wo_spaces = re.sub("^\s+", "", line_wo_spaces)
+        line_wo_spaces = re.sub("\s+", " ", line_wo_spaces)
+        line_wo_spaces = re.sub("\(\s+", "(", line_wo_spaces)
+        line_wo_spaces = re.sub("\s*,\s*", ", ", line_wo_spaces)
+        line_wo_spaces = re.sub("\s+\)", ")", line_wo_spaces)
+
+        for i in range(start, len(line_wo_spaces)):
+
+            if line_wo_spaces[i] == ";":
+
+                is_found = True
+                if line_wo_spaces[i - 1] == " ":
+                    line_wo_spaces = line_wo_spaces[0:i - 1] + ";"
+                break
+
+        if is_found is True:
+            break
+
+        start = len(line_wo_spaces) - 1
+        line_wo_spaces += " " + input("...> ")
+
+    return line_wo_spaces
+
+
+def parse(line):
+    if line[:6].lower() == "insert" or line[:11].lower() == "insert into":
+
+        line = line.replace(";", "")
+        for j in range(0, len(line)):
+            if line[j] == "(":
+
+                if line[-1] == ")":
+                    command = line[0:j - 1].split(" ")
+                    if len(command) != 2 and command[1].lower() != "into" or \
+                        len(command) != 3 and command[1].lower() == "into" or \
+                            len(command) == 1:
+
+                        return "Error: command includes no or too many arguments in name section."
+                    if command[1].lower() == "into":
+                        command = [command[0] + command[1]] + command[2:]
+                    if not re.match("[a-zA-Z]", command[1]):
+                        return "Error: keyword names have to start with letter, not number."
+                    args = line[j + 1:-1].split(", ")
+                    for k in range(0, len(args)):
+                        if args[k] == "" or len(re.findall("\"", args[k])) != 2:
+                            return "Error: invalid syntax in argument section."
+                        else:
+                            args[k] = args[k][1:-1]
+                    return command + args
+
+                else:
+                    return "Error: ) is missed or placed wrongly."
+        return "Error: ( is missed or placed wrongly."
+
+    elif line[:6].lower() == "create":
+
+        line = line.replace(";", "")
+        for j in range(0, len(line)):
+            if line[j] == "(":
+
+                if line[-1] == ")":
+                    if len(line[0:j-1].split(" ")) != 2:
+                        return "Error: command includes no or too many arguments in name section."
+                    command = line[0:j-1].split(" ")
+                    args = line[j+1:-1].split(", ")
+                    for k in range(0, len(args)):
+                        if args[k] == "":
+                            return "Error: invalid syntax in argument section."
+                        elif not re.match("[a-zA-Z]", args[k]):
+                            return "Error: keyword names have to start with letter, not number."
+
+                    return command + args
+
+                else:
+                    return "Error: ) is missed."
+        return "Error: ( is missed."
+
+    elif line[:11].lower() == "select from":
+
+        line = line.replace(";", "")
+        pattern = r'^[Ss][Ee][Ll][Ee][Cc][Tt]\s[Ff][Rr][Oo][Mm]\s[\w_]+'
+
+        if re.match(pattern+"$", line):
+            command = line.split(" ")
+            return [command[0] + command[1]] + [command[2]]
+        elif re.match(pattern+".+", line):
+            test_join = r'\s[Ll][Ee][Ff][Tt]_[Jj][Oo][Ii][Nn]\s[\w_]+\s[Oo][Nn]\s[\w_]+\s\=\s[\w_]+'
+            test_where = r'\s[Ww][Hh][Ee][Rr][Ee]\s[\w_]+\s[\=\<\>]\s(([\w_]+)|([\"]\w+[\"]))'
+            # print(re.match(test_join, line))
+            if not re.match(pattern+test_join+test_where+"$", line):
+
+                if re.match(pattern+test_join+"$", line):
+                    command = line.split(" ")
+                    return [command[0] + command[1]] + command[2:]
+                else:
+                    command = "Error: syntax error in LEFT_JOIN section."
+
+                if re.match(pattern+test_where+"$", line):
+                    command = line.split(" ")
+                    return [command[0] + command[1]] + command[2:]
+                else:
+                    command = "Error: syntax error in WHERE section."
+
+                return command
+            else:
+                command = line.split(" ")
+                command = [command[0] + command[1]] + [command[2:]]
+                return command
+
+        else:
+            return "Error: table name is not given correctly."
+    else:
+        print("Error: unknown command.")
