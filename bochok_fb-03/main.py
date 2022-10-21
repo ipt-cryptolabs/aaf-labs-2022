41864b498633f33a4a9ab8c32598b2b52a96309a
bochok26 <97351597+bochok26@users.noreply.github.com>
2022-10-21 23:31:45 +0300

    edited modules section

--- bochok_fb-03/main.py
+++ bochok_fb-03/main.py
@@ -1,15 +1,15 @@
 import re
 import functions
-import myparser
+import myparser1
 
 
 while True:
     line = input("   > ")
     if line != "":
-        result = myparser.make_normal(line)
+        result = myparser1.make_normal(line)
 
         print(result)
-        func = myparser.parse(result)
+        func = myparser1.parse(result)
         print(func)
         if func[0].lower() == "create":
             functions.create(func)
