a8279f3a12e5965de46c6e850cedf529832f05ea
bochok26 <97351597+bochok26@users.noreply.github.com>
2022-10-23 22:58:25 +0300

    error messages are now displayed correctly :^)

--- bochok_fb-03/main.py
+++ bochok_fb-03/main.py
@@ -1,20 +1,22 @@
-import re
 import functions
 import myparser1
 
-
 while True:
     line = input("   > ")
+    # for exiting the program, type "enter"
     if line != "":
         result = myparser1.make_normal(line)
 
+        # print(result)
         func = myparser1.parse(result)
-        
+        # print(func)
         if func[0].lower() == "create":
             functions.create(func)
         elif func[0][0:6].lower() == "insert":
             functions.insert(func)
         elif func[0][0:6].lower() == "select":
             functions.select(func)
+        else:
+            print(func)
     else:
         break
