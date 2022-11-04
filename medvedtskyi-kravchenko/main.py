aa3d909954a52e60f62252238b2e8ee25f8c02a7
kostyamed <kostyamed0003@gmail.com>
2022-11-04 13:39:29 +0200

    removed pandas and DELETE

--- medvedtskyi-kravchenko/main.py
+++ medvedtskyi-kravchenko/main.py
@@ -3,6 +3,4 @@ import db
 t1 = db.table("table1", ["id", "name", "bd"], ["", "", ""])
 t1.add_row(["1", "kost", "10"])
 t1.add_row(["2", "kost", "20"])
-print(t1.data)
-t1.delete_row(0)
-print(t1.data)
+t1.print_table()
