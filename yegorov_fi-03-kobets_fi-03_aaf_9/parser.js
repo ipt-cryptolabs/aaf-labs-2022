a4464f865cbc796d8aa0daba9f568f1d66df3a98
msverr <denis.s.kobets@gmail.com>
2022-11-05 14:20:46 +0200

    parser fixed, remove nodemon

--- yegorov_fi-03-kobets_fi-03_aaf_9/parser.js
+++ yegorov_fi-03-kobets_fi-03_aaf_9/parser.js
@@ -1,8 +1,4 @@
-const input = require('./input');
 const commands = require('./commands');
-const fs = require('fs');
-
-let commandLog = JSON.parse(fs.readFileSync('./log/commandLog.json', 'utf8'));
 
 const createReg = /^CREATE+\s+\b[a-zA-Z0-9_]+\b$/i;
 const insertReg = /^INSERT+\s+[a-zA-Z0-9_]+\s+"+[^"]+"$/i;
@@ -32,7 +28,19 @@ function ifValid(str){
     throw new Error("INVALID INPUT");
 }
 
-input().then(input => {
+function consoleInput() {
+    let buf = '';
+    process.stdin.on('data', data => {
+        buf += ' ' + data.toString().trim();
+        if (buf.endsWith(';')) {
+            parser(buf);
+            buf = '';
+        }
+    });
+}
+
+
+const parser = input => {
     let tokens = [];
     let token = ""
     try {
@@ -49,29 +57,22 @@ input().then(input => {
         switch(ifValid(tokens.join(" "))){
             case 0:
                 commands.create(tokens[1]);
-                commandLog.command.push("CREATE COMMAND");
                 break;
             case 1:
                 commands.insert(tokens[1], tokens.slice(2,));
-                commandLog.command.push("INSERT COMMAND");
                 break;
             case 2:
                 commands.print_index(tokens[1]);
-                commandLog.command.push("PRINT_INDEX COMMAND");
                 break;
             case 3:
                 commands.search(tokens[1]);
-                commandLog.command.push("SEARCH COMMAND");
                 break;
             case 4:
                 commands.searchWhere(tokens[1], tokens.slice(3));
-                commandLog.command.push("SEARCH WHERE COMMAND");
                 break;
             case 5:
                 let dashIndex = tokens.indexOf('-');
-
                 commands.searchWhereDash(tokens[1], tokens.slice(3, dashIndex), tokens.slice(dashIndex+1, ));
-                commandLog.command.push("SEARCH WHERE_DASH COMMAND");
                 break;
             case 6:
                 let numInHooks;
@@ -85,15 +86,12 @@ input().then(input => {
                 let numInHooksIndex = tokens.indexOf(numInHooks);
 
                 commands.searchWhereHooks(tokens[1], tokens.slice(3,numInHooksIndex), numInHooks.slice(1, numInHooks.length-1), tokens.slice(numInHooksIndex+1, ));
-                commandLog.command.push("SEARCH WHERE_HOOKS COMMAND");
                 break;
         }
         console.log(tokens);
-        fs.writeFileSync('./log/commandLog.json', JSON.stringify(commandLog));
     }catch(error){
-        commandLog.command.push("INVALID COMMAND");
-        fs.writeFileSync('./log/commandLog.json', JSON.stringify(commandLog));
         console.log(error);
     }
-});
+};
 
+consoleInput();
