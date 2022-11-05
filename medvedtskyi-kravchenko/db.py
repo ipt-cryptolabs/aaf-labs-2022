7042d3aaeea3bb3d2a89f9fdeef1712725896f23
kostyamed <kostyamed0003@gmail.com>
2022-11-05 17:05:21 +0200

    added 'select' and 'where' methods

--- medvedtskyi-kravchenko/db.py
+++ medvedtskyi-kravchenko/db.py
@@ -6,15 +6,48 @@ class table:
     
     def __init__(self, table_name: str, table_columns: list, table_params: list) -> None:
         self.name = table_name
+        cols = ["id"]
+        for i in table_columns:
+            cols.append(i)
+        self.columns = cols
         self.column_params = table_params
-        self.columns = table_columns
     
-    def add_row(self, row_data: list) -> None:
-        old_data = self.data
-        old_data.append(row_data)
-        self.data = old_data
-    
+    def new_row(self, row_data: list) -> None:
+        def add_row(rd_id: int, rd: list) -> None:
+            new_data = [str(rd_id)]
+            for i in rd:
+                new_data.append(i)
+            old_data = self.data
+            old_data.append(new_data)
+            self.data = old_data
+        if self.data != []:
+            row_id = int(self.data[-1][0]) + 1
+        else:
+            row_id = 0
+        add_row(row_id, row_data)
+            
     def print_table(self) -> None:
+        print(self.name)
         print(self.columns)
         for i in self.data:
             print(i)
+    
+    def get_row(self, row_id: int) -> list:
+        for s in self.data:
+            if int(s[0]) == row_id:
+                return s 
+        return []
+    
+    def where(self, column_name: str, data: str) -> int:
+        col_id = -1
+        for i, s in enumerate(self.columns):
+            if s == column_name:
+                col_id = i
+                break
+        if col_id == -1:
+            return col_id
+        for s in self.data:
+            if(s[col_id] == data):
+                return int(s[0])
+        return -1
+        
