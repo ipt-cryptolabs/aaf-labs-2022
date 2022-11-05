83162724713403606ef8855198678ca962ca98d9
kostyamed <kostyamed0003@gmail.com>
2022-11-05 20:52:11 +0200

    added all 3 commands to parser

--- medvedtskyi-kravchenko/parser/parser.py
+++ medvedtskyi-kravchenko/parser/parser.py
@@ -1,16 +1,21 @@
-from matplotlib.pyplot import table
 from pyparsing import *
 
 a = 'CREATE students (name, group, ggg, bbb, sss, aaa);'
+# a = 'SELECT FROM kost WHERE name = kosssst;'
+# a = 'INSERT INTO kot (huh, wtfimdoing, irgr);'
 
 table_name = (Word(alphas + '_'))('Name_Of_Table')
 column = Word(alphas + '_')
 column_names = (column + ZeroOrMore(Suppress(', ') + column))('modules')
-parse_module = 'CREATE' + table_name + Suppress('(') + column_names + Suppress(');')
+command_name = one_of(["CREATE", "INSERT INTO", "SELECT FROM"])('command')
+condition = (Word(alphas + '_') + Suppress('=') + Word(alphas + '_'))('cond')
+where_statement = (Suppress('WHERE') + condition + Suppress(';'))('wherest')
+create_and_insert_statement = (Suppress('(') + column_names + Suppress(');'))('crst')
+last = Or([where_statement, create_and_insert_statement])('laast')
+parse_module = command_name + table_name + last
 
 #print(parse_module.parseString(a))
 res = parse_module.parseString(a)
-mass = res.modules.asList()
-Name_Of_Table = res.Name_Of_Table
-print(res.modules.asList())
+print(res.command)
 print(res.Name_Of_Table)
+print(res.laast)
