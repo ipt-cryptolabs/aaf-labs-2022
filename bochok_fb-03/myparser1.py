85b73aa7443b7c8c097ae0a51a4aa259eb6bafb2
bochok26 <97351597+bochok26@users.noreply.github.com>
2022-11-06 00:10:36 +0200

    fixed parsing errors (remarks 2-5)

--- bochok_fb-03/myparser1.py
+++ bochok_fb-03/myparser1.py
@@ -9,6 +9,7 @@ def make_normal(line):
     while True:
         line_wo_spaces = re.sub("^\s+", "", line_wo_spaces)
         line_wo_spaces = re.sub("\s+", " ", line_wo_spaces)
+        line_wo_spaces = re.sub("\s*\(", " (", line_wo_spaces)
         line_wo_spaces = re.sub("\(\s+", "(", line_wo_spaces)
         line_wo_spaces = re.sub("\s*,\s*", ", ", line_wo_spaces)
         line_wo_spaces = re.sub("\s+\)", ")", line_wo_spaces)
@@ -27,14 +28,18 @@ def make_normal(line):
         if is_found is True:
             break
 
-        start = len(line_wo_spaces) - 1
+        start = len(line_wo_spaces)
         line_wo_spaces += " " + input("...> ")
 
     return line_wo_spaces
 
 
 def parse(line):
-    if line[:6].lower() == "insert" or line[:11].lower() == "insert into":
+
+    if line == ";":
+        return "Error: no command input."
+
+    elif line[:6].lower() == "insert" or line[:11].lower() == "insert into":
 
         line = line.replace(";", "")
         for j in range(0, len(line)):
@@ -42,22 +47,44 @@ def parse(line):
 
                 if line[-1] == ")":
                     command = line[0:j - 1].split(" ")
+
                     if len(command) != 2 and command[1].lower() != "into" or \
                         len(command) != 3 and command[1].lower() == "into" or \
                             len(command) == 1:
-
                         return "Error: command includes no or too many arguments in name section."
+
                     if command[1].lower() == "into":
                         command = [command[0] + command[1]] + command[2:]
-                    if not re.match("[a-zA-Z]", command[1]):
+
+                    if not re.match("[a-zA-Z]", command[1][0]):
                         return "Error: table names have to start with letter."
-                    args = line[j + 1:-1].split(", ")
-                    for k in range(0, len(args)):
-                        if args[k] == "" or len(re.findall("\"", args[k])) != 2:
-                            return "Error: invalid syntax in argument section."
-                        else:
-                            args[k] = args[k][1:-1]
-                    return command + args
+                    args = line[j + 1:-1]
+                    if args[0] != "\"" or args[-1] != "\"":
+                        return "Error: invalid syntax in argument section."
+
+                    args_l = args.split("\", \"")
+
+                    if len(args_l) == 1 and len(re.findall("\"", args_l[0])) == 2:
+                        args_l[0] = args_l[0][1:-1]
+                    elif len(args_l) > 1:
+                        for k in range(0, len(args_l)):
+                            if args_l[k] == "":
+                                return "Error: invalid syntax in argument section."
+                            elif k in range(1, len(args_l)-1) and len(re.findall("\"", args_l[k])) != 0:
+                                return "Error: invalid syntax in argument section."
+                            elif (k == 0 or k == len(args_l) - 1) and len(re.findall("\"", args_l[k])) == 1:
+                                if k == 0:
+                                    args_l[k] = args_l[k][1:]
+                                else:
+                                    args_l[k] = args_l[k][:-1]
+                            elif (k == 0 or k == len(args_l) - 1) and len(re.findall("\"", args_l[k])) > 1:
+                                return "Error: invalid syntax in argument section."
+                            else:
+                                args_l[k] = args_l[k][1:-1]
+                    else:
+                        return "Error: invalid syntax in argument section."
+
+                    return command + args_l
 
                 else:
                     return "Error: ) is missed or placed wrongly."
@@ -73,6 +100,7 @@ def parse(line):
                     if len(line[0:j-1].split(" ")) != 2:
                         return "Error: command includes no or too many arguments in name section."
                     command = line[0:j-1].split(" ")
+
                     if not re.match("[a-zA-Z]", command[1][0]):
                         return "Error: table names have to start with letter."
                     args = line[j+1:-1].split(", ")
@@ -98,7 +126,7 @@ def parse(line):
             return [command[0] + command[1]] + [command[2]]
         elif re.match(pattern+".+", line):
             test_join = r'\s[Ll][Ee][Ff][Tt]_[Jj][Oo][Ii][Nn]\s[\w_]+\s[Oo][Nn]\s[\w_]+\s\=\s[\w_]+'
-            test_where = r'\s[Ww][Hh][Ee][Rr][Ee]\s[\w_]+\s[\=\<\>]\s(([\w_]+)|([\"]\w+[\"]))'
+            test_where = r'\s[Ww][Hh][Ee][Rr][Ee]\s[\w_]+\s<\s(([\w_]+)|([\"]\w+[\"]))'
             # print(re.match(test_join, line))
             if not re.match(pattern+test_join+test_where+"$", line):
 
