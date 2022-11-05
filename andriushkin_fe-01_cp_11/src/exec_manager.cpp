e9fe64da7c33ec867968da7a570666f8818548d0
sanezek <sanezek@protonmail.com>
2022-11-05 17:36:11 +0200

    Tokenizer fix

--- andriushkin_fe-01_cp_11/src/exec_manager.cpp
+++ andriushkin_fe-01_cp_11/src/exec_manager.cpp
@@ -1,9 +1,7 @@
 #include "exec_manager.hpp"
 
-ExecManager::ExecManager():
-    os(std::cout), is(std::cin), running(true) {};
-ExecManager::ExecManager(std::ostream& os_, std::istream& is_):
-    os(os_), is(is_), running(true) {};
+ExecManager::ExecManager() : os(std::cout), is(std::cin), running(true){};
+ExecManager::ExecManager(std::ostream &os_, std::istream &is_) : os(os_), is(is_), running(true){};
 ExecManager::~ExecManager(){};
 
 bool ExecManager::IsRunning()
@@ -27,59 +25,59 @@ void ExecManager::RunLoop()
 std::stringstream ExecManager::FineReader()
 {
     std::stringstream ret;
-    size_t size = 0; 
+    size_t size = 0;
     os << ">>> ";
     char p = is.peek();
-    while(p != ';')
+    while (p != ';')
     {
-        if( p == '\n')
-        {   
-            if( size != 0) 
+        if (p == '\n')
+        {
+            if (size != 0)
                 os << "... ";
             else
                 os << ">>> ";
-        }   
+        }
         else
-            size +=1;
+            size += 1;
         ret << p;
         is.get();
-    
+
         p = is.peek();
     }
     os << "\n";
-    while( p != '\n')
+    while (p != '\n')
     {
         p = is.get();
     }
     return ret;
 }
 
-std::vector<std::string> ExecManager::Tokenize(std::stringstream& is)
+std::vector<std::string> ExecManager::Tokenize(std::stringstream &is)
 {
     std::string s;
     std::vector<std::string> out;
     char p = is.peek();
-    while((p != ';') & (p != EOF))
+    while ((p != ';') & (p != EOF))
     {
-        if( !( isalnum(p) | isspace(p) | p == ';' | p == ')' | p == '(' | p == '_' | p == ',' ))
-            throw std::invalid_argument("Unexpected symbol in input: "  
-                                + std::string(&p));
-        //std::cout << "check " << p << " : " << "ispunct: " << bool(ispunct(p)) << " p != '_': " << (p !='_') 
-        //    << " and " << (bool(ispunct(p)) & (p !='_')) << std::endl;
-        if( bool(ispunct(p)) & (p !='_'))
+        if (!(isalnum(p) | isspace(p) |
+              p == '-' | p == ';' | p == ')' | p == '(' | p == '_' | p == ','))
+            throw std::invalid_argument("Unexpected symbol in input: " + std::string(&p));
+        // std::cout << "check " << p << " : " << "ispunct: " << bool(ispunct(p)) << " p != '_': " << (p !='_')
+        //     << " and " << (bool(ispunct(p)) & (p !='_')) << std::endl;
+        if (bool(ispunct(p)) & (p != '_') & ( p != '-'))
         {
-            if( s.size() > 0)
+            if (s.size() > 0)
             {
                 out.push_back(s);
                 s = "";
             }
-            out.push_back(std::string(&p));
+            out.push_back(std::string(1,p));
         }
-        else if (! isspace(p))
+        else if (!isspace(p))
             s.push_back(p);
-        else 
+        else
         {
-            if( s.size() > 0)
+            if (s.size() > 0)
             {
                 out.push_back(s);
                 s = "";
@@ -88,36 +86,7 @@ std::vector<std::string> ExecManager::Tokenize(std::stringstream& is)
         is.get();
         p = is.peek();
     }
-    if( s.size() > 0)
+    if (s.size() > 0)
         out.push_back(s);
     return out;
 }
-
-
-/*
-Command ExecManager::RecognizeComm(const std::vector<std::string>& tokens)
-{
-    if( tokens[0] == "CREATE")
-    {
-        if( !(tokens.size() == 2))
-            throw std::invalid_argument("too much arguments");
-        if( !CheckVarName(tokens[1]))
-            throw std::invalid_argument("invalid var name");
-        return Command::CREATE;
-    }
-
-    //INSERT set_name (x, y);
-    else if( tokens[0] == "INSERT")
-    {
-        if( !(tokens.size() == 7))
-            throw std::invalid_argument("too much arguments");
-        if( !CheckVarName(tokens[1]))
-            throw std::invalid_argument("invalid var name");
-        // todo chech if set exist
-        if ( tokens[2] == "(" & tokens[4] == "," & tokens[6] == ")")
-            throw std::invalid_argument("invalid ");
-    } 
-    throw std::invalid_argument("unexpected command");
-    return Command::CREATE;
-}
-*/
