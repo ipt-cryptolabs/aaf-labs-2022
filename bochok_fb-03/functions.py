1989db07a58e7ba901d46f78df56adc6e9d61624
bochok26 <97351597+bochok26@users.noreply.github.com>
2022-11-06 00:08:18 +0200

    new output format in select function

--- bochok_fb-03/functions.py
+++ bochok_fb-03/functions.py
@@ -9,5 +9,15 @@ def insert(args):
 
 
 def select(args):
-    print(f"Selecting from {args[1]}...")
+    line = f"Selecting from table {args[1]}"
+    if len(args) == 2:
+        line += "..."
+    elif len(args) == 8:
+        line += f"\nleft_join table {args[3]} on columns: {args[5]}, {args[-1]}"
+    elif len(args) == 6:
+        line += f"\ncondition: {args[3]} {args[4]} {args[5]}"
+    else:
+        line += f"""\nleft_join table {args[3]} on columns {args[5]}, {args[7]}
+condition: {args[9]} {args[10]} {args[11]}"""
+    print(line)
     return
