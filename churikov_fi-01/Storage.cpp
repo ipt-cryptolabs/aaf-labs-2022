b0ea412502a2d571507be2ca9295dd74d6dd5f78
EvgeniyChurikov <zhenya.churikov@gmail.com>
2022-11-08 02:42:50 +0200

    Implement Storage class

--- churikov_fi-01/Storage.cpp
+++ churikov_fi-01/Storage.cpp
@@ -1,40 +1,91 @@
 #include "Storage.hpp"
 #include <sstream>
+#include <iomanip>
 
+
+Storage::Storage() {
+    tries = std::map<std::string, Trie>();
+}
+
+void Storage::create(CreateRequest request, std::stringstream &ss) {
+    const std::string &trie_name = request.getTrieName();
+    if (!tries.contains(trie_name)) {
+        tries.insert({trie_name, Trie()});
+        ss << "Trie " << trie_name << " has been created";
+    } else {
+        ss << "Trie " << trie_name << " already exist";
+    }
+}
+
+void Storage::insert(InsertRequest request, std::stringstream &ss) {
+    const std::string &trie_name = request.getTrieName(), &value = request.getValue();
+    if (tries.contains(trie_name)) {
+        tries[trie_name].insert(value);
+        ss << "String " << std::quoted(value) << " has been added to " << trie_name;
+    } else {
+        ss << "Trie " << trie_name << " does not exist";
+    }
+}
+
+void Storage::print_tree(PrintTreeRequest request, std::stringstream &ss) {
+    const std::string &trie_name = request.getTrieName();
+    if (tries.contains(trie_name)) {
+        ss << tries[trie_name].print();
+    } else {
+        ss << "Trie " << trie_name << " does not exist";
+    }
+}
+
+void Storage::contains(ContainsRequest request, std::stringstream &ss) {
+    const std::string &trie_name = request.getTrieName(), &value = request.getValue();
+    if (tries.contains(trie_name)) {
+        if (tries[trie_name].contains(value))
+            ss << "TRUE";
+        else
+            ss << "FALSE";
+    } else {
+        ss << "Trie " << trie_name << " does not exist";
+    }
+}
+
+void Storage::search(SearchRequest request, std::stringstream &ss) {
+    const std::string &trie_name = request.getTrieName();
+    if (tries.contains(trie_name)) {
+        std::vector<std::string> values = tries[trie_name].search();
+        if (!values.empty()) {
+            ss << std::quoted(values[0]);
+            for (int i = 1; i < values.size(); ++i) {
+                ss << ", " << std::quoted(values[i]);
+            }
+        } else {
+            ss << "Strings not found";
+        }
+    } else {
+        ss << "Trie " << trie_name << " does not exist";
+    }
+}
 
 std::string Storage::execute(const std::shared_ptr<Request> &request) {
     std::stringstream ss;
     switch (request->getCommandName()) {
         case CREATE: {
-            ss << "CREATE: "
-               << std::static_pointer_cast<CreateRequest>(request)->getTrieName()
-               << " trie created";
+            create(*std::static_pointer_cast<CreateRequest>(request), ss);
             break;
         }
         case INSERT: {
-            InsertRequest ir = *std::static_pointer_cast<InsertRequest>(request);
-            ss << "INSERT: "
-               << ir.getValue()
-               << " inserted to " << ir.getTrieName() << " trie";
+            insert(*std::static_pointer_cast<InsertRequest>(request), ss);
             break;
         }
         case PRINT_TREE: {
-            ss << "PRINT_TREE: "
-               << std::static_pointer_cast<PrintTreeRequest>(request)->getTrieName()
-               << " trie printed";
+            print_tree(*std::static_pointer_cast<PrintTreeRequest>(request), ss);
             break;
         }
         case CONTAINS: {
-            ContainsRequest cr = *std::static_pointer_cast<ContainsRequest>(request);
-            ss << "CONTAINS: "
-               << cr.getValue()
-               << " may be contained in " << cr.getTrieName() << " trie";
+            contains(*std::static_pointer_cast<ContainsRequest>(request), ss);
             break;
         }
         case SEARCH: {
-            ss << "SEARCH: "
-               << std::static_pointer_cast<SearchRequest>(request)->getTrieName()
-               << " trie searched";
+            search(*std::static_pointer_cast<SearchRequest>(request), ss);
             break;
         }
         default:
--- churikov_fi-01/Storage.hpp
+++ churikov_fi-01/Storage.hpp
@@ -2,11 +2,28 @@
 #define CHURIKOV_FI_01_STORAGE_HPP
 
 #include "Request.hpp"
+#include "Trie.hpp"
 #include <memory>
+#include <map>
 
 
 class Storage {
+private:
+    std::map<std::string, Trie> tries;
+
+    void create(CreateRequest request, std::stringstream &ss);
+
+    void insert(InsertRequest request, std::stringstream &ss);
+
+    void print_tree(PrintTreeRequest request, std::stringstream &ss);
+
+    void contains(ContainsRequest request, std::stringstream &ss);
+
+    void search(SearchRequest request, std::stringstream &ss);
+
 public:
+    Storage();
+
     std::string execute(const std::shared_ptr<Request> &request);
 };
 
