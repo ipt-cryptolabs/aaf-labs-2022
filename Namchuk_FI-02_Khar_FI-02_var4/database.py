3be6897c0e0d5ab280686d29f26fcdb233b6444b
Solution <maxim.namchuk@gmail.com>
2022-11-09 16:00:31 +0200

    ELIF

--- Namchuk_FI-02_Khar_FI-02_var4/database.py
+++ Namchuk_FI-02_Khar_FI-02_var4/database.py
@@ -19,18 +19,41 @@ class DataBase:
         elif command[2] == "WHERE":
             table = self.tables[command[1]]
             values = []
-            index_column = table.columns.index(command[3])
+            index_column_1 = table.columns.index(command[3])
             if command[4][0] == '"':
                 command[4] = command[4][1:-1]
                 i = 0
                 while i < len(table.values):
-                    if table.values[i][index_column] > command[4]:
+                    if table.values[i][index_column_1] > command[4]:
                         values.append(table.values[i])
                     i += 1
-                table = [table.columns, *values]
-                tab = PrettyTable(table[0])
-                tab.add_rows(table[1:])
-                print(tab)
+                if values == []:
+                    table = [table.columns, *table.values]
+                    tab = PrettyTable(table[0])
+                    tab.add_rows(table[1:])
+                    print(tab)
+                else:
+                    table = [table.columns, *values]
+                    tab = PrettyTable(table[0])
+                    tab.add_rows(table[1:])
+                    print(tab)
+            else:
+                index_column_2 = table.columns.index(command[4])
+                i = 0
+                while i < len(table.values):
+                    if table.values[i][index_column_1] > table.values[i][index_column_2]:
+                        values.append(table.values[i])
+                    i += 1
+                if values == []:
+                    table = [table.columns, *table.values]
+                    tab = PrettyTable(table[0])
+                    tab.add_rows(table[1:])
+                    print(tab)
+                else:
+                    table = [table.columns, *values]
+                    tab = PrettyTable(table[0])
+                    tab.add_rows(table[1:])
+                    print(tab)
         elif (command[2] == "JOIN") and (len(command) == 4):
             table_1 = self.tables[command[1]]
             table_2 = self.tables[command[3]]
@@ -58,8 +81,64 @@ class DataBase:
                 tab.add_rows(table[1:])
                 print(tab)
         elif (command[2] == "JOIN") and (command[4] == "ON") and (len(command) == 7):
-            pass
+            table_1 = self.tables[command[1]]
+            table_2 = self.tables[command[3]]
+            index_column_1 = table_1.columns.index(command[5])
+            index_column_2 = table_2.columns.index(command[6])
+            if len(table_1.values) == len(table_2.values):
+                values = []
+                i = 0
+                while i < len(table_1.values):
+                    if table_1.values[i][index_column_1] == table_2.values[i][index_column_2]:
+                        values.append(table_1.values[i] + table_2.values[i])
+                    i += 1
+                table = [[*table_1.columns, *table_2.columns], *values]
+                tab = PrettyTable(table[0])
+                tab.add_rows(table[1:])
+                print(tab)
+            else:
+                values = []
+                i = 0
+                while i < len(table_1.values):
+                    j = 0
+                    while j < len(table_2.values):
+                        values.append(table_1.values[i] + table_2.values[j])
+                        j += 1
+                    i += 1
+                table = [[*table_1.columns, *table_2.columns], *values]
+                tab = PrettyTable(table[0])
+                tab.add_rows(table[1:])
+                print(tab)
         elif (command[2] == "JOIN") and (command[4] == "ON") and (command[7] == "WHERE"):
             pass
         elif (command[2] == "JOIN") and (command[4] == "WHERE"):
-            pass
+            table_1 = self.tables[command[1]]
+            table_2 = self.tables[command[3]]
+            index_column_1 = table_1.columns.index(command[3])
+            if command[6][0] == '"':
+                command[6] = command[6][1:-1]
+                i = 0
+                if len(table_1.values) == len(table_2.values):
+                    while i < len(table_1.values):
+                        if table.values[i][index_column_1] > command[4]:
+                            values.append(table.values[i])
+                        i += 1
+                    if values == []:
+                        table = [table.columns, *table.values]
+                        tab = PrettyTable(table[0])
+                        tab.add_rows(table[1:])
+                        print(tab)
+                    else:
+                        table = [table.columns, *values]
+                        tab = PrettyTable(table[0])
+                        tab.add_rows(table[1:])
+                        print(tab)
+        '''
+        CREATE cats (name, group);
+        CREATE owners (name2, group2);
+        INSERT INTO cats ("1", "Vasya");
+        INSERT INTO cats ("2", "Vasya");
+        INSERT INTO owners ("1", "Vasya");
+        INSERT INTO owners ("3", "Vasya");
+        select from cats join owners on name = name2;
+        '''
