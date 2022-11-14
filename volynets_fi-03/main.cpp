66214ac06b79ee89f1cc63cc00e067a1dc7f80a6
Serhii04 <94004153+Serhii04@users.noreply.github.com>
2022-11-14 17:22:50 +0200

    Update main.cpp

--- volynets_fi-03/main.cpp
+++ volynets_fi-03/main.cpp
@@ -6,25 +6,9 @@
 using namespace std;
 
 int main(){
-    // testAll();
-
     DB_terminal db;
     
-    db.callCommand("CREATE    cats (age INDEXED, name, dish);");
-    db.callCommand("INSERT INTO cats (\"7\", \"Murzik\", \"Sausages\");");
-    db.callCommand("insert InTO cats (\"1\", \"Kyselut\", \"Sausages, with  double chease\");");
-    db.callCommand("INSERT INTO cats (\"3\", \"Gladislav\", \"Brokoli\");");
-    db.callCommand("INSERT INTO cats (\"5\", \"Kysimir\", \"fingers\");");
-    db.callCommand("INSERT INTO cats (\"8\", \"Moushebor\", \"mouse\");");
-    db.callCommand("INSERT INTO cats (\"13\", \"Vladivan\", \"Divanchic\");");
-    db.callCommand("INSERT INTO cats (\"13\", \"Vladivan\", \"Divan\");");
-    db.callCommand("INSERT INTO cats (\"6\", \"Pyzochos\", \"Cabels\");");
-    std::cout << db.callCommand("SELECT FROM cats;") << std::endl;
-    std::cout << db.callCommand("SELECT FROM cats WHERE age > \"5\";") << std::endl;
-    std::cout << db.callCommand("SELECT FROM cats WHERE age <= \"5\";") << std::endl; 
-    std::cout << db.callCommand("SELECT FROM cats WHERE name > \"Ky\" ORDER_BY name DESC, dish ASC;") << std::endl;
-
-    // db.start();
+    db.start();
 
     /////////////////////////////////////////////////////////
     //////////////////// Excample commands: /////////////////
