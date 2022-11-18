9204d97290d26911fae482f8d8e310e9b60d44de
bochok26 <97351597+bochok26@users.noreply.github.com>
2022-11-18 21:30:53 +0200

    Update main.py

--- bochok_fb-03/main.py
+++ bochok_fb-03/main.py
@@ -1,5 +1,7 @@
-import functions
+import db_functions
 import myparser1
+
+crude_db = db_functions.DB()
 
 while True:
     line = input("   > ")
@@ -9,10 +11,10 @@ while True:
     func = myparser1.parse(result)
     # print(func)
     if func[0].lower() == "create":
-        functions.create(func)
+        crude_db.create(func)
     elif func[0][0:6].lower() == "insert":
-        functions.insert(func)
+        crude_db.insert_row(func)
     elif func[0][0:6].lower() == "select":
-        functions.select(func)
+        crude_db.select(func)
     else:
         print(func)
