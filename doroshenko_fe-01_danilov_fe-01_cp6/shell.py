9b1e49f9523d6c113090c2b26d3a46e688dd601b
Morarin1 <vovadorosenko1@gmail.com>
2023-01-07 22:03:40 +0200

    fixed the comments of the first deadline

--- doroshenko_fe-01_danilov_fe-01_cp6/shell.py
+++ doroshenko_fe-01_danilov_fe-01_cp6/shell.py
@@ -1,13 +1,20 @@
 from run import run
+import readline
 
-
+text = ''
 while True:
-    text = input('shit_DB > ')
-   
-    result, error = run(text)
+    line = input('shit_DB > ')
+    buffer = readline.get_line_buffer()
+    if buffer:
+        line += '\n' + buffer
+    text += line
 
-    if error:
-        print(error.as_string())
-    else:
-        pass
-        # print(result)
+    if not line.strip():
+        try:
+          result, error = run(text)
+          text = ''
+          if error:
+              print(error.as_string())
+        except Exception as e:
+          print("An error occurred:", e)
+          continue
