bffdcf760f44a0d3719a4944dadd4b5b5a324e49
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-01-09 16:53:53 +0200

    create CMakeList.txt

---
+++ volynets_fi-03/build/CMakeFiles/volynets_fi-03.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/serhii/Documents/Кур Уроки/ОAA (в.)/labs/aaf-labs-2022/volynets_fi-03/main.cpp" "CMakeFiles/volynets_fi-03.dir/main.cpp.o" "gcc" "CMakeFiles/volynets_fi-03.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ volynets_fi-03/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/serhii/Documents/Кур Уроки/ОAA (в.)/labs/aaf-labs-2022/volynets_fi-03
+# Build directory: /home/serhii/Documents/Кур Уроки/ОAA (в.)/labs/aaf-labs-2022/volynets_fi-03/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
